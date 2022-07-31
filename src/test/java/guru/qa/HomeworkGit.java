9347f0b873ef3ab2830fe37573195115a2275243
Kirill <kirillcnv@gmail.com>
2022-07-31 16:11:36 +0300

    add test

---
+++ src/test/java/guru/qa/HomeworkGit.java
@@ -0,0 +1,20 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HomeworkGit {
+
+    @Test
+
+    void test() {
+
+        Assertions.assertTrue(2==2);
+        Assertions.assertTrue(2!=22);
+    }
+}
+
+
+
+
+
